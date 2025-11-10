d43bd4c509a82047874ad28a9a6b0ae880135378
Nicolas Urrego <133616018+nicurrego@users.noreply.github.com>
2025-11-10 14:12:09 +0900

    loading page done.

--- components/WelcomeScreen.tsx
+++ components/WelcomeScreen.tsx
@@ -10,7 +10,7 @@ const SoundIcon: React.FC<{ isMuted: boolean }> = ({ isMuted }) => (
     {isMuted ? (
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15zM17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2" />
     ) : (
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.536 8.464a5 5 0 010 7.072M12 6v12m-2-10a2 2 0 01-2-2V8a2 2 0 012-2h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L8.414 15H6a2 2 0 01-2-2v-4a2 2 0 012-2h2.414l.172-.172a5 5 0 017.072 0z" />
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" />
     )}
   </svg>
 );
